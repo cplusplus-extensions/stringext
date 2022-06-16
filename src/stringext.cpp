ec963c61578d06ae1974076a71d7c944f09d7ca2
marck283 <86911602+marck283@users.noreply.github.com>
2022-06-17 01:21:36 +0200

    Added warning on 'join' function

--- src/stringext.cpp
+++ src/stringext.cpp
@@ -31,10 +31,12 @@ std::vector<std::string> split(const std::string& str, const std::string& delim)
 }
 
 /**
- * @brief This function joins two strings into one resulting string.
+ * @brief This function joins two strings into one resulting string, appending every character in the second string to the end of the first string.
  * 
  * @param v1 The first string
  * @param v2 The second string
+ * 
+ * @warning As of C++20, there are standard functions that allow two strings to be joined. This hand-made version is therefore suitable for all C++ standard versions prior to C++20.
  */
 void join(std::string& v1, const std::string& v2) noexcept {
     for(const auto& i: v2) {
