c1406c4659531f05404da73ce6856bc5035a5ad8
marck283 <86911602+marck283@users.noreply.github.com>
2022-07-23 17:47:49 +0200

    Aggiunto controllo versione standard C++ e aggiunta nuova implementazione di tutte le funzioni

--- src/stringext.cpp
+++ src/stringext.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+
+#if __cplusplus >= 201703L
+#include <string_view>
 
 /**
- * @brief This library's purspose is to provide the programmer with additional potentially useful functions that can be used on strings.
+ * @brief This function joins two strings into one resulting string, appending every character in the second string to the end of the first string.
+ * 
+ * @param v1 The first string
+ * @param v2 The second string
+ * 
+ * @warning As of C++20, there are standard functions that allow two strings to be joined. This hand-made version is therefore suitable for all C++ standard versions prior to C++20 that implement std::for_each.
+ */
+void join(std::string& v1, const std::string& v2) noexcept {
+    std::for_each(v1.begin(), v1.end(), [&v1](char i) {
+        v1.push_back(i);
+    });
+}
+
+#endif
+
+
+/**
+ * @brief This library's purpose is to provide the programmer with additional potentially useful functions that can be used on strings.
  * You are free to suggest any improvement in the "Discussions" page or in the "Issues" page, provided that you also give a working example
  * to explain how you are solving the problem!
  */
@@ -10,6 +32,7 @@
 /**
  * @brief This function splits the given string into a vector of strings using the given delimiter.
  * 
+ * @warning This function uses the STL algorithm "std::for_each"
  * @param str The string to split
  * @param delim The delimiter used to split the string
  * @return std::vector<std::string> The resulting vector as described above.
@@ -17,14 +40,14 @@
 std::vector<std::string> split(const std::string& str, const std::string& delim) noexcept {
     std::string str1;
     std::vector<std::string> v;
-    for(const char& it : str) {
+    std::for_each(str.begin(), str.end(), [&v, &str1, delim](char it) {
         if(delim.find(it) == std::string::npos) {
-            str1 += it;
+            str1.push_back(it);
         } else {
             v.push_back(str1);
-            str1 = "";
+            str1.clear();
         }
-    }
+    });
     v.push_back(str1);
 
     return v;
@@ -36,7 +59,7 @@ std::vector<std::string> split(const std::string& str, const std::string& delim)
  * @param v1 The first string
  * @param v2 The second string
  * 
- * @warning As of C++20, there are standard functions that allow two strings to be joined. This hand-made version is therefore suitable for all C++ standard versions prior to C++20.
+ * @warning As of C++17, std::for_each has been introduced. This hand-made version is therefore suitable for all C++ standard versions prior to C++17.
  */
 void join(std::string& v1, const std::string& v2) noexcept {
     for(const auto& i: v2) {
